e6eb4757a372149cba4633e7ded258c72a4ba7c3
User <ug-fca-gig@ug.edu.ec>
2025-05-07 09:27:53 -0500

    CUADRADO Y RECTANGULO

---
+++ RECTANGULO.py
@@ -0,0 +1,12 @@
+from FiguraGeometrica import FiguraGeometrica
+
+
+class Rectangulo(FiguraGeometrica):
+    def __init__(self, alto, ancho):
+        super().__init__(alto, ancho)
+
+    def area(self):
+        return self._alto * self._ancho
+
+    def __str__(self):
+        return f"Rectángulo de alto {self._alto}, ancho {self._ancho}, área {self.area()}"
